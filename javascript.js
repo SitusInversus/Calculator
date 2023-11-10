ba0d8559b80402c0e78038b59d8420e19b3f23c4
Christian on Mac <spiegeleulein@gmail.com>
2023-11-10 11:43:56 +0100

    Add Basic Operator Functions In JS

--- javascript.js
+++ javascript.js
@@ -5,3 +5,32 @@ const deleteButton = document.querySelector('[data-delete]')
 const euqalSignButton = document.querySelector('[data-equal-sign]')
 const previousResult = document.querySelector('[data-previous-result]')
 const currentoperation = document.querySelector('[data-current-operation]')
+
+function add (a,b) {
+    return a+b
+}
+function subtract (a,b) {
+    return a-b
+}
+function multiply (a,b) {
+    return a*b
+}
+function divide (a,b) {
+    if (b===0) return Infinity
+    return a/b
+}
+
+function operate(operand, a, b) {
+    //since I will use .innerText to get the value of button:
+    a = Number(a)
+    b = Number(b)
+
+    switch (operand) {
+        case '+': return add(a,b)
+        case '-': return subtract(a,b)
+        case '*': return multiply(a,b)
+        case '÷': return divide(a,b)
+        default: return 
+    }
+}
+
